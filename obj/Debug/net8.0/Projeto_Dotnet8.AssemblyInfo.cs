ad0a3fe35f9d581e0a72ab3610e992935c8a5817
Leo <leonardo.a.oliveira6@ba.estudante.senai.br>
2025-11-04 16:36:16 -0300

    04.11.25

--- obj/Debug/net8.0/Projeto_Dotnet8.AssemblyInfo.cs
+++ obj/Debug/net8.0/Projeto_Dotnet8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_Dotnet8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f2bcbfc5e761d8c8849b17349e5edc9bd9fbd00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68ab66093c67ac8510ce07e0d472f98af257d2cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_Dotnet8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_Dotnet8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
